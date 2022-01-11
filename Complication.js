56356ae2a980fef79ee7f55b1aa27cf5a9659b65
Andreas Rozek <A.Rozek@gmx.de>
2022-01-11 06:42:05 +0100

    Create Complication.js

---
+++ Complication.js
@@ -0,0 +1,12 @@
+(function () {
+  let Weekdays = [ 'Su','Mo','Tu','We','Th','Fr','Sa' ];
+
+  exports.draw = function draw (x,y, Radius, Settings) {
+    g.setColor(Settings.Foreground === 'Theme' ? g.theme.fg : Settings.Foreground || '#000000');
+    g.setFont('Vector', 18);
+    g.setFontAlign(0,0);
+
+    let today = new Date();
+    g.drawString(Weekdays[today.getDay()], x,y);
+  };
+})();
